d9f3edb406395a8b7c046f3c0c9036ce2700dbdd
Aryan Singh <as10071999@gmail.com>
2024-08-31 16:41:18 +0530

    added header in blog

--- src/Blog.tsx
+++ src/Blog.tsx
@@ -1,9 +1,15 @@
 type Props = {};
+import illustration from "./assets/illustration-article.svg";
 
 const Blog = (props: Props) => {
   return (
     <div className="w-[384px] h-[522px] rounded-[20px] bg-[#FFFFFF] border  border-black box-shadow">
-      Blog
+      {/* Header */}
+      <div className="p-[24px]">
+        <img src={illustration} alt="" className="rounded-[10px]" />
+      </div>
+      {/* Body  */}
+      <div></div>
     </div>
   );
 };
